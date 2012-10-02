cacfeaa1081ee91ac2cf48261330fae90bb10052
Barney Fox <barneyfoxuk@gmail.com>
2012-10-02 12:45:58 +0100

    added conditional for versions of IE8 and bellow

--- blk.jquery.msie.bg-pos.js
+++ blk.jquery.msie.bg-pos.js
@@ -5,26 +5,26 @@
  * http://blkcreative.com
  *
  * @use: keep this comment with the code
- * 
+ *
  */
 (function ( $ ) {
 
-if ( $.browser.msie ) {
+if ( $.browser.msie && $.browser.version <= 8 ) {
 	$.cssHooks.backgroundPosition = {
 		get: function ( elem, computed, extra ) {
 			var $elem	= $( elem ),
 				posX	= $elem.css( "backgroundPositionX" ),
 				posY	= $elem.css( "backgroundPositionY" );
-			
+
 			return posX + " " + posY;
 		},
-		
+
 		set: function ( elem, value ) {
 			var $elem	= $( elem ),
 				values	= value.split( " " ),
 				posX	= values[0],
 				posY	= values[1];
-				
+
 			$elem.css({
 				"backgroundPositionX": posX,
 				"backgroundPositionY": posY
